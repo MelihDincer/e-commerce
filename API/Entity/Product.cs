4038f734c5b9b9253f805e0d73cc8f41a0137f66
Melih DİNÇER <m_dincer41@hotmail.com>
2025-01-25 19:00:21 +0300

    E-commerce projesi oluşturuldu. Temel konfigürasyon ayarları yapılarak proje swagger ile başarıyla ayağa kaldırıldı. -İlk entity sınıfı oluşturuldu.

---
+++ API/Entity/Product.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entity;
+
+public class Product 
+{
+    public int ProductId { get; set; }
+    [Required]
+    public string ProductName { get; set; }
+    public string Description { get; set; }
+    public decimal ProductPrice{ get; set; }
+    public bool IsActive { get; set; }
+    public string ImageUrl { get; set; }
+    public int Stock { get; set; }
+}

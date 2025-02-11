84bca1d7ddd3c80a40f0f470e236cea84db69d4b
Melih DİNÇER <m_dincer41@hotmail.com>
2025-02-11 23:10:17 +0300

    11 Şubat 2025 -React projesi eklenerek useState kullanımı ve API consume gibi çalışmalar yapıldı.

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddDbContext<DataContext>(options => {
     var connectionString = config.GetConnectionString("defaultConnection");
     options.UseSqlite(connectionString);
 });
+
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -27,6 +29,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(opt => {
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 app.UseAuthorization();
 
 app.MapControllers();

e309d9e1c73fc11a168e7afc7abaab0e96cc8b71
Melih Dincer <m_dincer41@hotmail.com>
2025-02-27 22:51:51 +0300

    27 Şubat 2025 -First Commit. Ekleme ve silme işlemleri yapıldı.

--- API/Entity/Card.cs
+++ API/Entity/Card.cs
@@ -5,6 +5,32 @@ public class Card
     public int CardId {get; set;}
     public string CustomerId {get; set;} = null!;
     public List<CardItem> CardItems { get; set; } = new List<CardItem>();
+    public void AddItem(Product product, int quantity) 
+    {
+        var item = CardItems.Where(c => c.ProductId == product.ProductId).FirstOrDefault();
+        if(item == null)
+        {
+            CardItems.Add(new CardItem {Product = product, Quantity = quantity});
+        }
+        else
+        {
+            item.Quantity += quantity;
+        }
+    }
+
+    public void DeleteItem(int productId, int quantity)
+    {
+        var item = CardItems.Where(c => c.ProductId == productId).FirstOrDefault();
+        if(item == null)
+        {
+            return;
+        }
+        item.Quantity -= quantity;
+        if(item.Quantity == 0)
+        {
+            CardItems.Remove(item);
+        }
+    }
 }
 
 public class CardItem
---
+++ API/Migrations/20250227190704_AddCard.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCard : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    CardId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CustomerId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.CardId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CardItem",
+                columns: table => new
+                {
+                    CardItemId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CardId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardItem", x => x.CardItemId);
+                    table.ForeignKey(
+                        name: "FK_CardItem_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "CardId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CardItem_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CardItem_CardId",
+                table: "CardItem",
+                column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CardItem_ProductId",
+                table: "CardItem",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CardItem");
+
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
